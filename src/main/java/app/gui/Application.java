1d574113fe679ca00706160efbab4864d88206d7
He Sun <sh.sun@mail.utoronto.ca>
2025-10-06 10:47:53 -0400

    fixed button

--- src/main/java/app/gui/Application.java
+++ src/main/java/app/gui/Application.java
@@ -389,6 +389,7 @@ public class Application {
         theCard.add(new JLabel("The course you want to calculate the team average for:"));
         theCard.add(courseField);
         theCard.add(getAverageButton);
+        theCard.add(getTopButton);
         theCard.add(leaveTeamButton);
         theCard.add(resultLabel);
         return theCard;
