4c5360177feffbb0eba3fbfa4c541d09aa35abbd
Tamako F <tamako.fan@mail.utoronto.ca>
2025-10-06 10:29:36 -0400

    config changes

--- src/main/java/app/Config.java
+++ src/main/java/app/Config.java
@@ -65,5 +65,4 @@ public class Config {
         return new GetAverageGradeUseCase(gradeDataBase);
     }
 
-    // TODO Task 4: add code for the new GetTopGradeUseCase following the same pattern as the other use cases above.
 }
